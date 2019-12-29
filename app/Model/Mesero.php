a8d7b8d06ae9cb164ad312e6cde1ef962977c3b9
Marcos JRS <marcosjrs80@gmail.com>
2019-12-29 16:41:22 +0100

    Añadir comprobacion de DNI unico en model Mesero

--- app/Model/Mesero.php
+++ app/Model/Mesero.php
@@ -4,7 +4,11 @@ class Mesero extends AppModel
 {
     public $validate = array(
         'dni'=>array(
-            'Campo requerido' => array( 'rule'=>'notEmpty' )
+            'Campo requerido' => array( 'rule'=>'notEmpty' ),
+            'unico' => array(
+                'rule' => 'isUnique',
+                'message' => 'Ya existe otro mesero con este DNI'
+            )
         ),
         'nombre'=>array(
             'Campo requerido' => array( 'rule'=>'notEmpty' ),
